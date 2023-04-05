2a19d2d4a1d18dbc6bfb21860e0b8969b04b5426
akhi9661 <akhiraj9661@gmail.com>
2023-04-06 01:57:03 +0530

    Bump version: 0.0.2 → 0.1.0

--- ocm2/__init__.py
+++ ocm2/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Akhilesh Kumar"""
 __email__ = 'akhiraj9661@gmail.com'
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 
 from .ocm2 import ExportSubdatasets, metaInfo, GetExtent, do_georef
