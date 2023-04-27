5650ad28ab3c175b793bafd4b02b0791613f52c3
akhi9661 <akhiraj9661@gmail.com>
2023-04-28 02:28:51 +0530

    Updated __init__

--- ocm2/__init__.py
+++ ocm2/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Akhilesh Kumar"""
 __email__ = 'akhiraj9661@gmail.com'
 __version__ = '0.1.0'
 
-from .ocm2 import ExportSubdatasets, metaInfo, GetExtent, do_reflectance, do_georef
+from .ocm2 import *
